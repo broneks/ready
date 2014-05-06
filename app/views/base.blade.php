f1dcd210f41ec70845703a223f6e166671881e94
Bszulc <bronekszulc@gmail.com>
2014-05-06 16:55:17 -0400

    initial

---
+++ app/views/base.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+</head>
+<body>
+	<div id="wrap">
+		@yield('main')
+	</div>
+</body>
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('title')
+	Hello
+@stop
+
+@section('main')
+	<h1>Hello</h1>
+@stop
